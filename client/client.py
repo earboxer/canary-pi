36d528b005952088660aebfc80cac0519cf976fe
Jesse Kuntz <jessekuntz96@gmail.com>
2018-04-26 15:12:36 -0400

    - made progress

--- client/client.py
+++ client/client.py
@@ -3,23 +3,37 @@ import paho.mqtt.client as mqtt
 import os
 import time
 
-BROKER = "iot.cs.calvin.edu"
+BROKER = "???"
 PORT = 1883
 QOS = 0
 
+# Callback when a message is published
+def on_publish(client, userdata, mid):
+    print("MQTT data published")
 # Callback when a connection has been established with the MQTT broker
 def on_connect(client, userdata, rc, *extra_params):
- print('Connected with result code='+str(rc))
+    print('Connected with result code='+str(rc))
 # Callback when client receives a PUBLISH message from the broker
 def on_message(client, data, msg):
- if msg.topic == "pi1":
-    print("Received message: Note = ", int(msg.payload))
-    os.system('sonic_pi play ' + int(msg.payload))
+    if msg.topic == "pi1":
+        print("Received message: Note = ", int(msg.payload))
+        os.system('sonic_pi play ' + int(msg.payload))
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
+client.on_publish = on_publish
 
 client.connect(BROKER, PORT, 60)
 client.subscribe("pi1", qos=QOS)
 client.loop_start()
+
+try:
+    while True:
+        # add a duration or stop this at some point
+        os.system('arecord --device=hw:1,0 --format S16_LE --rate 44100 -c1 test.wav')
+        client.publish("pi1/newfile", './test.wav')
+        time.sleep(3)
+except KeyboardInterrupt:
+    print('Done')
+    client.disconnect()
