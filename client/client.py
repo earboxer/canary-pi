72c963d5c1117ba6f6118007b77cfb71e5518280
Zach DeCook <zachdecook@gmail.com>
2018-05-05 16:47:13 -0400

    - Client: Clean up output

--- client/client.py
+++ client/client.py
@@ -12,6 +12,7 @@ import os
 import time
 import sys
 import datetime
+import commands
 
 BROKER = sys.argv[2]
 PORT = 1883
@@ -20,7 +21,7 @@ QOS = 0
 # Callback when client receives a PUBLISH message from the broker
 def on_message(client, data, msg):
     if msg.topic == sys.argv[1] + "/queue":
-        print("Received message: Note = ", int(msg.payload))
+        print("Received message: " + str(int(msg.payload)) + ". Playing note...")
         if (int(msg.payload) > 40):
             os.system('sonic_pi play ' + str(int(msg.payload)) + ', sustain: 1.25')
 
@@ -36,8 +37,11 @@ try:
     while True:
         # Modulo so it will overwrite old files
         wavname = str(int(time.time() * 10)%100) + sys.argv[1] + ".wav"
-        os.system('arecord --device=hw:1,0 --format S16_LE --rate 48000 -c1 -d 1 test.wav')
-        os.system('scp test.wav pi@' + sys.argv[2] + ':~/' + wavname)
+	sys.stdout.write( "Recording... " )
+        commands.getoutput('arecord --device=hw:1,0 --format S16_LE --rate 48000 -c1 -d 1 test.wav')
+	sys.stdout.write( "Copying file " + wavname + "... " )
+        commands.getoutput('scp test.wav pi@' + sys.argv[2] + ':~/' + wavname)
+	print( "Copied." )
         client.publish(sys.argv[1] + "/newfile", wavname)
 	time.sleep(.1)
 except KeyboardInterrupt:
