ca87d9b49ea24074438e34f124761db2a7ddea56
Jesse Kuntz <jessekuntz96@gmail.com>
2018-05-05 14:41:22 -0400

    finishing touches?

--- client/client.py
+++ client/client.py
@@ -11,6 +11,7 @@ import paho.mqtt.client as mqtt
 import os
 import time
 import sys
+import datetime
 
 BROKER = sys.argv[2]
 PORT = 1883
@@ -24,7 +25,7 @@ def on_connect(client, userdata, rc, *extra_params):
     print('Connected with result code='+str(rc))
 # Callback when client receives a PUBLISH message from the broker
 def on_message(client, data, msg):
-    if msg.topic == sys.argv[1]:
+    if msg.topic == sys.argv[1] + "/queue":
         print("Received message: Note = ", int(msg.payload))
         os.system('sonic_pi play ' + int(msg.payload))
 
@@ -35,16 +36,16 @@ client.on_publish = on_publish
 
 client.connect(BROKER, PORT, 60)
 client.subscribe(sys.argv[1], qos=QOS)
+client.subscribe(sys.argv[1] + "/queue", qos=QOS)
 client.loop_start()
 
 try:
     while True:
     # add a duration or stop this at some point
-	wavname = sys.argv[1] + '.wav'
-        os.system('arecord --device=hw:1,0 --format S16_LE --rate 48000 -c1 -d 5 test.wav')
+	wavname = int(round(time.time())) + sys.argv[1] + ".wav"
+    os.system('arecord --device=hw:1,0 --format S16_LE --rate 48000 -c1 -d 3 test.wav')
 	os.system('scp test.wav pi@' + sys.argv[2] + ':~/' + wavname)
-        client.publish(sys.argv[1] + "/newfile", wavname)
-        time.sleep(3)
+    client.publish(sys.argv[1] + "/newfile", wavname)
 except KeyboardInterrupt:
     print('Done')
     client.disconnect()
